d1bdd60620d15fa7a7dc45d4dacc0cd457addbf7
Paul Chavez <paulchavezromero20@gmail.com>
2024-06-04 21:00:57 -0500

    chore: openai added

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
   async orthographyCheck(orthographyDto: OrthographyDto) {
     const { prompt } = orthographyDto;
 
-    return await orthographyCheckUseCase({ prompt });
+    return await orthographyCheckUseCase(this.openai, { prompt });
   }
 }
